6894d18a12307f56404c2f3125f417889b62720b
Gerard Deschamps <FR22765@L83738.fr01.awl.atosorigin.net>
2016-02-24 05:25:37 +0100

    init project

---
+++ app/main.ts
@@ -0,0 +1,4 @@
+import {bootstrap}    from 'angular2/platform/browser'
+import {WpsApp} from './wps.component'
+
+bootstrap(WpsApp);
---
+++ app/wps.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+@Component({
+  selector: 'wps-app',
+  template: '<h1>Imag Wps</h1>'
+})
+export class WpsApp {
+}
