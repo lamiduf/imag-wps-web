c50e246a2223b979e7ded51a9f2ce44ee53e4c14
Gerard Deschamps <FR22765@L83738.fr01.awl.atosorigin.net>
2016-02-25 06:30:33 +0100

    Routing - commit travail

---
+++ app/accueil/accueil.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'accueil-cmp',
+  templateUrl: 'app/accueil/accueil.component.html'
+})
+export class AccueilCmp {
+}
--- app/wps.component.ts
+++ app/wps.component.ts
@@ -1,7 +1,22 @@
 import {Component} from 'angular2/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import {AccueilCmp} from './accueil/accueil.component';
+
 @Component({
-  selector: 'wps-app',
-  template: '<h1>Imag Wps</h1>'
+    selector: 'wps-app',
+    templateUrl: 'app/wps.component.html',
+    directives: [ROUTER_DIRECTIVES],
+    providers: [
+        ROUTER_PROVIDERS
+    ]
 })
+@RouteConfig([
+  {
+    path: '/accueil',
+    name: 'Accueil',
+    component: AccueilCmp,
+    useAsDefault: true
+  }
+])  
 export class WpsApp {
 }
